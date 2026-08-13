d6e49b2ed29d3bfcae1e6d98413f8930639eb856
Saranjib Kuanar <102132535+saranjib@users.noreply.github.com>
2026-08-14 01:51:37 +0530

    Update variables in variable.tf
    Removed default value for resource group name and added storage account name variable.

--- host-website-on-blob-storage/variable.tf
+++ host-website-on-blob-storage/variable.tf
@@ -1,11 +1,14 @@
 variable "resource_group_name" {
   description = "Azure Resource Group name"
   type        = string
-  default     = "rg-frontdoor-cdn-demo"
-}
+ }
 
 variable "location" {
   description = "Azure region"
   type        = string
   default     = "Central India"
+}
+variable "storage_account_name" {
+  description = "Globally unique Storage Account name"
+  type        = string
 }
