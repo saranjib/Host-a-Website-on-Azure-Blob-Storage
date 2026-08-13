6fc03ede5a44610acc6d7b9ee2e41370625d6079
Saranjib Kuanar <102132535+saranjib@users.noreply.github.com>
2026-08-14 01:48:06 +0530

    Add Azure resources for static website hosting
    Define resources for Azure Blob Storage and static website hosting.

---
+++ host-website-on-blob-storage/main.tf
@@ -0,0 +1,52 @@
+resource "azurerm_resource_group" "" {
+  name     = var.resource_group_name
+  location = var.location
+
+}
+
+
+# ---------------------------------------------------------
+# Azure Storage Account
+# ---------------------------------------------------------
+
+resource "azurerm_storage_account" "website-storage" {
+  name                     = var.storage_account_name
+  resource_group_name      = azurerm_resource_group.host-web-blob-project.name
+  location                 = azurerm_resource_group.host-web-blob-project.location
+  account_tier              = "Standard"
+  account_replication_type = "LRS"
+
+  min_tls_version = "TLS1_2"
+
+  static_website {
+    index_document = "index.html"
+    error_404_document = "index.html"
+  }
+
+}
+
+
+# ---------------------------------------------------------
+# Website HTML
+# ---------------------------------------------------------
+
+resource "azurerm_storage_blob" "website-blob-index" {
+  name                   = "index.html"
+  storage_account_name   = azurerm_storage_account.frontdoor-cdn-storage.name
+  storage_container_name = "$web"
+
+  type         = "Block"
+  source       = "${path.module}/website/index.html"
+  content_type = "text/html"
+}
+
+# ---------------------------------------------------------
+# Enable Static Website Hosting
+# ---------------------------------------------------------
+
+resource "azurerm_storage_account_static_website" "static_site" {
+  storage_account_id = azurerm_storage_account.frontdoor-cdn-storage.id
+  index_document     = "index.html"
+  error_404_document = "404.html"
+}
+
