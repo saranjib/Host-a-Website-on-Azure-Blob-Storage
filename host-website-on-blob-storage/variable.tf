d72f63237a832e67f1bf2ddb5d1695622da9fc37
Saranjib Kuanar <102132535+saranjib@users.noreply.github.com>
2026-08-14 01:50:37 +0530

    Add variables for resource group and location

--- host-website-on-blob-storage/variable.tf
+++ host-website-on-blob-storage/variable.tf
@@ -1,1 +1,11 @@
+variable "resource_group_name" {
+  description = "Azure Resource Group name"
+  type        = string
+  default     = "rg-frontdoor-cdn-demo"
+}
 
+variable "location" {
+  description = "Azure region"
+  type        = string
+  default     = "Central India"
+}
