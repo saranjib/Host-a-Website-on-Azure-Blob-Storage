32460c67df94eec6528f05b126f1f0afd725074a
Saranjib Kuanar <102132535+saranjib@users.noreply.github.com>
2026-08-15 20:35:23 +0530

    Add function to display success message for website

---
+++ host-website-on-blob-storage/website/script.js
@@ -0,0 +1,8 @@
+function showMessage() {
+
+    const message = document.getElementById("message");
+
+    message.innerHTML =
+        "✅ Website is successfully running from Azure Blob Storage!";
+
+}
