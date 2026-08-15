67293f6f6f71342b45f8df017bf2e9d7ad438294
Saranjib Kuanar <102132535+saranjib@users.noreply.github.com>
2026-08-15 20:32:30 +0530

    Add random string for unique storage account names
    Added a random string resource for unique storage account names and updated references to the storage account.

--- host-website-on-blob-storage/main.tf
+++ host-website-on-blob-storage/main.tf
@@ -1,4 +1,4 @@
-resource "azurerm_resource_group" "" {
+resource "azurerm_resource_group" "host-web-blob-project" {
   name     = var.resource_group_name
   location = var.location
 
@@ -9,11 +9,18 @@ resource "azurerm_resource_group" "" {
 # Azure Storage Account
 # ---------------------------------------------------------
 
+# Declare the random_string resource
+resource "random_string" "random" {
+  length  = 6
+  special = false
+  upper   = false
+}
+
 resource "azurerm_storage_account" "website-storage" {
-  name                     = var.storage_account_name
+  name                     = "webhostproj${random_string.random.result}" # Must be globally unique
   resource_group_name      = azurerm_resource_group.host-web-blob-project.name
   location                 = azurerm_resource_group.host-web-blob-project.location
-  account_tier              = "Standard"
+  account_tier             = "Standard"
   account_replication_type = "LRS"
 
   min_tls_version = "TLS1_2"
@@ -32,7 +39,7 @@ resource "azurerm_storage_account" "website-storage" {
 
 resource "azurerm_storage_blob" "website-blob-index" {
   name                   = "index.html"
-  storage_account_name   = azurerm_storage_account.frontdoor-cdn-storage.name
+  storage_account_name   = azurerm_storage_account.website-storage.name
   storage_container_name = "$web"
 
   type         = "Block"
@@ -45,7 +52,7 @@ resource "azurerm_storage_blob" "website-blob-index" {
 # ---------------------------------------------------------
 
 resource "azurerm_storage_account_static_website" "static_site" {
-  storage_account_id = azurerm_storage_account.frontdoor-cdn-storage.id
+  storage_account_id = azurerm_storage_account.website-storage.id
   index_document     = "index.html"
   error_404_document = "404.html"
 }
