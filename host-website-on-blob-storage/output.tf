d7d92fc1b09fe27d9bc1decd37297e45de86645d
Saranjib Kuanar <102132535+saranjib@users.noreply.github.com>
2026-08-15 20:31:37 +0530

    Add outputs for resource group and storage account

--- host-website-on-blob-storage/output.tf
+++ host-website-on-blob-storage/output.tf
@@ -1,1 +1,12 @@
+output "resource_group_name" {
+  value = azurerm_resource_group.host-web-blob-project.name
+}
+
+output "storage_account_name" {
+  value = azurerm_storage_account.website-storage.name
+}
+
+output "static_website_url" {
+  value = azurerm_storage_account.website-storage.primary_web_endpoint
+}
 
